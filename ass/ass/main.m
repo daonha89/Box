68a2b48645ce3169f877bf9c6b613816c2a5250a
FSU17_FX-RD <fsu17_fx-rd@FSU17-FX-RDs-Mac-mini.local>
2014-04-11 14:05:05 +0700

    start

---
+++ ass/ass/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ass
+//
+//  Created by FSU17_FX-RD on 4/11/14.
+//  Copyright (c) 2014 FSU17_FX-RD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
